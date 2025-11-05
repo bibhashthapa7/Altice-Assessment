cb0873883c2f8dec8bba128888e7308dfd3106e7
Bibhash Thapa <bibhashthapa7@gmail.com>
2025-11-05 15:55:39 -0500

    load env var after base dir

--- django-backend/movie_api/settings.py
+++ django-backend/movie_api/settings.py
@@ -14,11 +14,11 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv() # Load environment variables from .env file
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load environment variables from .env file
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
